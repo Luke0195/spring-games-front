a7bcb51ac7af62ccce6793990f166aa6dcdf6df7
Lucas Santos <lucas201273@gmail.com>
2023-07-12 08:45:52 -0300

    fix: eslint conflicts

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,6 @@
-export default function Home() {
+import React from 'react'
+
+export default function Home(): React.ReactNode {
   return (
     <div className="bg-slate-200 h-screen w-screen flex-col align-middle">
       <h2> Spring Games</h2>
