dd1af9b6e58a016d263045dcbefe1027ec56a760
Lucas Santos <lucas201273@gmail.com>
2023-07-12 19:10:53 -0300

    chore: config tailwind

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,8 +2,14 @@ import React from 'react'
 
 export default function Home(): React.ReactNode {
   return (
-    <div className="bg-slate-200 h-screen w-screen flex-col align-middle">
-      <h2> Spring Games</h2>
-    </div>
+    <>
+      <div className="bg-purple-800 h-screen flex flex-col  justify-center">
+        <div className="w-full bg-red p-2 flex align-middle justify-center">
+          <h2 className="text-4xl text-purple-100 font-semibold uppercase  align-middle bg-black ">
+            Spring games 2
+          </h2>
+        </div>
+      </div>
+    </>
   )
 }
