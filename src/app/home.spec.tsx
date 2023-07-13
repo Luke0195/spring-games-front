2388b1c19da8a3d48b29eed82afba4e2321cb560
Lucas Santos <lucas201273@gmail.com>
2023-07-12 21:51:47 -0300

    refactor: move sut creation to a factory method

--- src/app/home.spec.tsx
+++ src/app/home.spec.tsx
@@ -1,14 +1,22 @@
 import React from 'react'
-import { render, screen } from '@testing-library/react'
+import { render, screen, type RenderResult } from '@testing-library/react'
 import Home from './page'
+
+interface SutTypes {
+  sut: RenderResult
+}
+
+const makeSut = (): SutTypes => ({
+  sut: render(<Home />),
+})
 
 describe('App Component', () => {
   it('Ensure App Component be render correctly', () => {
-    render(<Home />)
+    makeSut()
   })
 
   it('Ensure that navigation button to be enabled', () => {
-    render(<Home />)
+    makeSut()
     const button = screen.getByRole('button', {
       name: /entrar/i,
     }) as HTMLButtonElement
