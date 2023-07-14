4b8305838a80ef163e0c5c03969d6518869b1ae9
Lucas Santos <lucas201273@gmail.com>
2023-07-14 07:03:23 -0300

    test: ensure heading 1 be render with correct value

--- src/app/signin/page.spec.tsx
+++ src/app/signin/page.spec.tsx
@@ -1,9 +1,17 @@
 import React from 'react'
-import { render } from '@testing-library/react'
+import { render, screen } from '@testing-library/react'
 import SignIn from './page'
 
 describe('SignIn Page', () => {
   it('Should render component correctly', () => {
     render(<SignIn />)
+  })
+
+  it('Should render heading 1 with correct value', () => {
+    render(<SignIn />)
+    const heading = screen.getByRole('heading', {
+      level: 1,
+    })
+    expect(heading.innerHTML).toBe(' Entre com o seu NickName')
   })
 })
