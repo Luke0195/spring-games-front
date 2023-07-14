62ea90fb5985517169655f4d041e668bd36bec75
Lucas Santos <lucas201273@gmail.com>
2023-07-14 06:53:25 -0300

    test: ensure signin page be render correctly

--- src/app/signin/page.spec.tsx
+++ src/app/signin/page.spec.tsx
@@ -1,1 +1,9 @@
 import React from 'react'
+import { render } from '@testing-library/react'
+import SignIn from './page'
+
+describe('SignIn Page', () => {
+  it('Should render component correctly', () => {
+    render(<SignIn />)
+  })
+})
