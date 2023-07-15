cebd0ce4469dd8735aae5c2ef5276b8a4bc2f5f1
Lucas Santos <lucas201273@gmail.com>
2023-07-15 08:52:46 -0300

    refactor: move signin form to a component folder

---
+++ src/app/signin/components/Form/index.tsx
@@ -0,0 +1,23 @@
+import { InputRoot, ButtonRoot } from '@/components/Form'
+import { FiMail, FiLogIn } from 'react-icons/fi'
+import Link from 'next/link'
+
+export function Form() {
+  return (
+    <form className="flex flex-col justify-center items-center py-3">
+      <InputRoot.InputWrapper>
+        <InputRoot.InputIcon icon={FiMail} color="#ccc" size={20} />
+        <InputRoot.InputContent placeholder="Entre com o seu Nick Name" />
+      </InputRoot.InputWrapper>
+      <ButtonRoot.ButtonWrapper className="w-96 h-11 bg-purple-600 rounded text-white my-6">
+        <ButtonRoot.ButtonContent> Salvar </ButtonRoot.ButtonContent>
+      </ButtonRoot.ButtonWrapper>
+      <Link
+        href={'Não possui conta'}
+        className="flex items-center hover:text-gray-700">
+        <span className="px-2 text-gray-400 "> Não possui conta</span>
+        <FiLogIn className="text-gray-400" />
+      </Link>
+    </form>
+  )
+}
--- src/app/signin/page.tsx
+++ src/app/signin/page.tsx
@@ -1,8 +1,7 @@
 'use client'
-import { InputRoot, ButtonRoot } from '@/components'
-import { FiMail, FiLogIn } from 'react-icons/fi'
+
 import React from 'react'
-import Link from 'next/link'
+import { Form } from './components'
 
 export default function SignIn() {
   return (
@@ -11,21 +10,7 @@ export default function SignIn() {
         <h1 className="text-gray-400 font-semibold text-2xl w-full text-center py-2">
           Entre com o seu Login
         </h1>
-        <form className="flex flex-col justify-center items-center py-3">
-          <InputRoot.InputWrapper>
-            <InputRoot.InputIcon icon={FiMail} color="#ccc" size={20} />
-            <InputRoot.InputContent placeholder="Entre com o seu Nick Name" />
-          </InputRoot.InputWrapper>
-          <ButtonRoot.ButtonWrapper className="w-96 h-11 bg-purple-600 rounded text-white my-6">
-            <ButtonRoot.ButtonContent> Salvar </ButtonRoot.ButtonContent>
-          </ButtonRoot.ButtonWrapper>
-          <Link
-            href={'Não possui conta'}
-            className="flex items-center hover:text-gray-700">
-            <span className="px-2 text-gray-400 "> Não possui conta</span>
-            <FiLogIn className="text-gray-400" />
-          </Link>
-        </form>
+        <Form />
       </div>
     </div>
   )
