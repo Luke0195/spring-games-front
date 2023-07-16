2418199f7d02e87508ce1eb2d232add33e84aeba
Lucas Santos <lucas201273@gmail.com>
2023-07-16 09:33:15 -0300

    feat: implements styled signin page

--- src/app/signin/page.tsx
+++ src/app/signin/page.tsx
@@ -1,13 +1,16 @@
 'use client'
-
-import React from 'react'
+import { Logo } from '@/components'
 import { Form } from './components'
+import React from 'react'
 
 export default function SignIn() {
   return (
     <div className="w-full h-screen flex items-center justify-center align-middle">
-      <div className="w-3/12  py-2 items-center  justify-center align-middle ">
-        <h1 className="text-gray-400 font-semibold text-2xl w-full text-center py-2">
+      <div className="w-3/12  py-2 flex-col align-middle items-center justify-center ">
+        <div className="w-full flex items-center justify-center">
+          <Logo />
+        </div>
+        <h1 className="text-purple-300  font-semibold text-xl w-full text-center py-2 ">
           Entre com o seu Login
         </h1>
         <Form />
--- src/app/signin/signin.spec.tsx
+++ src/app/signin/signin.spec.tsx
@@ -1,6 +1,8 @@
 import React from 'react'
+
 import SignIn from './page'
 import { render, screen, type RenderResult } from '@testing-library/react'
+import '@testing-library/jest-dom'
 
 const makeSut = (): RenderResult => {
   return render(<SignIn />)
@@ -19,12 +21,17 @@ describe('SignIn Page', () => {
     expect(heading.innerHTML).toBe('Entre com o seu Login')
   })
 
+  it('Shoulder render logo be in signin page', () => {
+    makeSut()
+    const logoComponent = screen.getByText(/gamespot/i)
+    expect(logoComponent).toBeInTheDocument()
+  })
+
   it('Should  start signin form with initial values', () => {
     makeSut()
     const input = screen.getByPlaceholderText(
       /entre com o seu nick name/i
     ) as HTMLInputElement
-    console.log('Innner htl', input)
     const button = screen.getByRole('button') as HTMLButtonElement
     expect(input.innerHTML).toBe('')
     expect(input.defaultValue).toBe('')
