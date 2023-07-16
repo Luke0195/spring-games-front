c2fef59d090269d29a977312a6f55d96ae56e259
Lucas Santos <lucas201273@gmail.com>
2023-07-16 18:16:28 -0300

    test: pipeline ci/cd

--- src/app/signup/page.tsx
+++ src/app/signup/page.tsx
@@ -1,3 +1,7 @@
 export default function SignUp() {
-  return <h1> Tela de Registro</h1>
+  return (
+    <div className="h-screen bg-purple-700">
+      <h1> Tela de Registro</h1>
+    </div>
+  )
 }
