f95caf2eff587f806068295372ef87686c0793cd
Lucas Santos <lucas201273@gmail.com>
2023-07-17 16:45:39 -0300

    chore: add cypress to e2e tests

---
+++ cypress.config.ts
@@ -0,0 +1,11 @@
+import { defineConfig } from 'cypress'
+
+module.exports = defineConfig({
+  e2e: {
+    setupNodeEvents(on, config) {
+      // implement node event listeners here
+    },
+    specPattern: 'cypress/e2e/**/*.cy.{js,jsx,ts,tsx}',
+    supportFile: false,
+  },
+})
---
+++ cypress/e2e/home.cy.ts
@@ -0,0 +1,5 @@
+describe('template spec', () => {
+  it('Ensure user provided a correct url to visited the website', () => {
+    cy.visit('http://localhost:3000')
+  })
+})
