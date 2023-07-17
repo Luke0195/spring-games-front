123ac54370d8ea541bbfc3234b1c7ef6b57d631b
Lucas Santos <lucas201273@gmail.com>
2023-07-17 08:43:28 -0300

    test: ci/cd

--- src/app/signup/page.tsx
+++ src/app/signup/page.tsx
@@ -1,7 +1,10 @@
 export default function SignUp() {
   return (
-    <div className="h-screen bg-purple-700">
-      <h1> Tela de Registro</h1>
+    <div className="h-screen bg-white">
+      <h1 className="text-slate-500 text-xl  w-full text-center">
+        {' '}
+        Tela de Registro
+      </h1>
     </div>
   )
 }
