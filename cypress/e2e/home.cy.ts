0f7054a6f3406440cc11a65e258f544fcb5a2264
Lucas Santos <lucas201273@gmail.com>
2023-07-18 08:28:04 -0300

    test: ensure navigate button to be enabled

--- cypress/e2e/home.cy.ts
+++ cypress/e2e/home.cy.ts
@@ -1,5 +1,10 @@
-describe('template spec', () => {
-  it('passes', () => {
+describe('Home ', () => {
+  it('Ensure user visit baseurl ', () => {
     cy.visit('http://localhost:3000')
+  })
+
+  it('Ensure navigate button to be enabled', () => {
+    cy.visit('http://localhost:3000')
+    cy.get('button').should('not.have.attr', 'disabled')
   })
 })
