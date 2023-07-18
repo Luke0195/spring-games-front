89736e1c810ad59ff2c89d0a8d74cbf40ee1901d
Lucas Santos <lucas201273@gmail.com>
2023-07-18 08:37:51 -0300

    test: change scripts

--- cypress.config.ts
+++ cypress.config.ts
@@ -3,11 +3,19 @@ import { defineConfig } from 'cypress'
 module.exports = defineConfig({
   video: false,
   supportFolder: 'cypress/support',
+
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
     baseUrl: 'http://localhost:3000',
     fixturesFolder: 'cypress/fixtures',
+  },
+
+  component: {
+    devServer: {
+      framework: 'next',
+      bundler: 'webpack',
+    },
   },
 })
